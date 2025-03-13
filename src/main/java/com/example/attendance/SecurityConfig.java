43c43b5d363b6e334b7a713c0ca4d9b26b2f90f4
UCHIDAnobuhiro <92204227+UCHIDAnobuhiro@users.noreply.github.com>
2025-03-13 16:21:56 +0900

    fix:securityconfigの修正

--- src/main/java/com/example/attendance/SecurityConfig.java
+++ src/main/java/com/example/attendance/SecurityConfig.java
@@ -20,14 +20,14 @@ public class SecurityConfig {
 						.requestMatchers("/css/**", "/js/**", "/images/**").permitAll() // 静的リソースは常に許可
 						.requestMatchers("/", "/login", "/signup").permitAll() // 全ユーザーに開放するUR
 						.requestMatchers("/admin/**").hasRole("ADMIN") // 管理者ロールにのみ許可
-						.requestMatchers("/worktime").authenticated() // `worktime` を認証必須にする
+						.requestMatchers("/worktime/**").authenticated() // `worktime` を認証必須にする
 						.anyRequest().authenticated() // 他のエンドポイントは認証が必要
 				);
 		// ログインフォームの設定
 		http.formLogin(login -> login
 				.loginPage("/login") // カスタムログインページ
 				.usernameParameter("mail") // フォームの `name="mail"` に対応
-				.defaultSuccessUrl("/worktime", true) // ログイン成功後の遷移先
+				.defaultSuccessUrl("/worktime/home", true) // ログイン成功後の遷移先
 				.failureHandler(authenticationFailureHandler()) // 認証失敗時のハンドラーを適用
 				.permitAll());
 
