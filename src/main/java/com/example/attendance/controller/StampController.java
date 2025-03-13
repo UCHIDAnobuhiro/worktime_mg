c093a936778266ffe747cb6e2128628005c3b4ea
Ouchinitsu <ouchinitsu@gmail.com>
2025-03-13 19:01:21 +0900

    update:stampのUI作成完了
    3/13 19:00 Ou
    thymleafなどは追加されてない、基本のボタンと時間の表示のみ

--- src/main/java/com/example/attendance/controller/StampController.java
+++ src/main/java/com/example/attendance/controller/StampController.java
@@ -1,13 +1,22 @@
 package com.example.attendance.controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StampController {
 
 	@GetMapping("/worktime/stamp")
-	public String showStamp() {
+	public String showStamp(Model model) {
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+		model.addAttribute("nowDate", now.format(dateFormatter));
+		model.addAttribute("nowTime", now.format(timeFormatter));
 		return "stamp";
 	}
 
