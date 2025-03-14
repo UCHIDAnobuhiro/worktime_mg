a90efc53767451ce46b74091303eb79d6c8a003f
UCHIDAnobuhiro <92204227+UCHIDAnobuhiro@users.noreply.github.com>
2025-03-14 16:30:21 +0900

    feat:ログイン時にユーザ情報を渡す

--- src/main/java/com/example/attendance/service/UsersService.java
+++ src/main/java/com/example/attendance/service/UsersService.java
@@ -1,5 +1,26 @@
 package com.example.attendance.service;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import com.example.attendance.model.Users;
+import com.example.attendance.repository.UsersRepository;
+
+@Service
 public class UsersService {
+	private final UsersRepository usersRepository;
+
+	public UsersService(UsersRepository usersRepository) {
+		this.usersRepository = usersRepository;
+	}
+
+	public Users getLoggedInUser() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication != null && authentication.getPrincipal() instanceof Users) {
+			return (Users) authentication.getPrincipal();
+		}
+		return null;
+	}
 
 }
