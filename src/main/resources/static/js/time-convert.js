04ef6a2d5aaadda5d4caa4506e9a7b19f8802844
Ouchinitsu <ouchinitsu@gmail.com>
2025-03-17 15:13:36 +0900

    fixed:変更ボタンを押すときに時間の整形を行うことにする
    3/17 14:00 Ou

--- src/main/resources/static/js/time-convert.js
+++ src/main/resources/static/js/time-convert.js
@@ -1,20 +1,15 @@
-//編集画面のデータ処理。　HH:mmからHH:mm:ssに変更
 const timeInputs = document.querySelectorAll('.editTime');
+const submitBtn = document.getElementById('submit_time');
 
-timeInputs.forEach(input => {
-	//inputだけではデータがうまく修正されない可能性があるため、changeの時も修正する。
-	input.addEventListener('input', function() {
+submitBtn.addEventListener('click', function(event) {
+	// 监听提交按钮的点击事件，处理时间格式为HH:mm:ss
+	timeInputs.forEach(input => {
 		let value = input.value;
 
+		// 如果输入的时间格式为HH:mm且没有秒数，自动补充秒数
 		if (value.length === 5) {
-			input.value = value + ":00";
+			input.value = value + ":00";  // 将HH:mm转换为HH:mm:ss
 		}
-	});
-
-	input.addEventListener('change', function() {
-		let value = input.value;
-		if (value.length === 5) {
-			input.value = value + ":00";
-		}
+	console.log(input.value);
 	});
 });
