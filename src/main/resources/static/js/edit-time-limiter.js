f1231ae3845fec2f904852ae94585a0583fb2109
Ouchinitsu <ouchinitsu@gmail.com>
2025-03-19 14:21:09 +0900

    fixed：コードの整形とコマンドの修正

--- src/main/resources/static/js/edit-time-limiter.js
+++ src/main/resources/static/js/edit-time-limiter.js
@@ -1,13 +1,13 @@
 //開始時刻が終了時刻より遅い場合はエラーメッセージの表示とデータ変更の抑制
 //どちらのみ入力された場合は、controllerで抑制
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded",  ()=> {
     const startTimeInput = document.getElementById("start_time");
     const endTimeInput = document.getElementById("end_time");
 
-    startTimeInput.addEventListener("input", function () {
+    startTimeInput.addEventListener("input",  ()=> {
         endTimeInput.min = startTimeInput.value;
     });
-	endTimeInput.addEventListener("input", function () {
+	endTimeInput.addEventListener("input",  ()=> {
 	    startTimeInput.max = endTimeInput.value;
 	});
 });
--- src/main/resources/static/js/time-convert.js
+++ src/main/resources/static/js/time-convert.js
@@ -1,14 +1,15 @@
 const timeInputs = document.querySelectorAll('.editTime');
 const submitBtn = document.getElementById('submit_time');
 
-submitBtn.addEventListener('click', function(event) {
-	// 监听提交按钮的点击事件，处理时间格式为HH:mm:ss
+//変更ボタンが押下時に時間を整形
+submitBtn.addEventListener('click', ()=> {
+	
 	timeInputs.forEach(input => {
 		let value = input.value;
 
-		// 如果输入的时间格式为HH:mm且没有秒数，自动补充秒数
+		//HH:mmの場合は秒数を追加
 		if (value.length === 5) {
-			input.value = value + ":00";  // 将HH:mm转换为HH:mm:ss
+			input.value = value + ":00"; 
 		}
 	console.log(input.value);
 	});
